dc72eb1270f981ea3975ed5c6464ffe6385483cb
wunjahboris <wunjahchuwa6@gmail.com>
2022-09-13 06:01:17 -0700

    header

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
  * main - Prints _putchar followed by a new line
  *
  * Return: Always 0
  */
-int main()
+int main(void)
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, sz;
@@ -13,8 +13,8 @@ int main()
 	sz = sizeof(str) / sizeof(int);
 	for (count = 0; count < sz; count++);
 	{
-		putchar(str[count]);
+		_putchar(str[count]);
 	}
-	putchar('\n');
+	_putchar('\n');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#ifindef _MAIN_H_
+#define _MAIN_H_
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int add (int, int);
+void times_table(void);
+int print_sign(int n);
+int _isalpha(int c);
+int _abs(int);
+void print_times_table(int n);
+int print_last_digit(int);
+void jack_bauer(void);
+void print_to_98(int n);
+
+#endif
