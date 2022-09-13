6ab616ebacf213778d29bb1b167379f0855a7e44
wunjahboris <wunjahchuwa6@gmail.com>
2022-09-13 05:43:40 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+/**
+ * main - Prints _putchar followed by a new line
+ *
+ * Return: Always 0
+ */
+int main()
+{
+	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++);
+	{
+		putchar(str[count]);
+	}
+	putchar('\n');
+	return (0);
+}
