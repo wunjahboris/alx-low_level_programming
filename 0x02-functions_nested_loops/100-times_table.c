1a89a2e087d47405b786ac007c63c1100bda53a4
wunjahboris <wunjahchuwa6@gmail.com>
2022-09-13 08:51:00 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,22 +17,18 @@ void print_times_table(int n)
 		{
 			for (b = 0; b <= n; b++)
 			{
-				c = a * b;
-				d = c / 100;
-				e = (c / 10) % 10;
+				c = a * b; d = c / 100; e = (c / 10) % 10;
 				f = (c % 100) % 10;
 				if (b == 0)
 				{
 					_putchar('0');
 				}
-				else if
-					(c < 10)
+				else if (c < 10)
 				{
 					_putchar(' '); _putchar(' ');
 					_putchar('0' + f);
 				}
-				else if
-					(c < 100)
+				else if (c < 100)
 				{
 					_putchar(' ');
 					_putchar('0' + e); _putchar('0' + f);
