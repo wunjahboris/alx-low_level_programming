9de6f57bcea8c2b51906a04bf992c9adcb1da765
wunjahboris <wunjahchuwa6@gmail.com>
2022-09-13 06:24:45 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,16 +5,16 @@
  *
  * Return: Always 0
  */
-int main(void)
+nt main(void)
 {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, sz;
 
 	sz = sizeof(str) / sizeof(int);
-for (count = 0; count < sz; count++);
-{
+	for (count = 0; count < sz; count++)
+	{
 		_putchar(str[count]);
 	}
 	_putchar('\n');
-	return (0);
+	return(0);
 }
